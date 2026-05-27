f481c9381ed7e307f5e1d61ccd42520e6c906480
jtozura <joe@ozura.io>
2026-05-26 20:39:24 -0400

    add ppt

--- BlazorPayMultiAPIDemo/src/PaymentGatewayApi/obj/Debug/net9.0/PaymentGatewayApi.AssemblyInfo.cs
+++ BlazorPayMultiAPIDemo/src/PaymentGatewayApi/obj/Debug/net9.0/PaymentGatewayApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentGatewayApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653c9b19012029f4497bc14c63be409af06096d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8c8f76cd2b67c87c817bc8b2ee3b98fb5e4a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentGatewayApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentGatewayApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
